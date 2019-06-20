0de6429fc7cedd0affea859d0976b95360b7e10b
ATeal <arteal89@gmail.com>
2019-06-20 07:57:17 -0400

    Updated aliases

--- oh-my-zsh/aliases.zsh
+++ oh-my-zsh/aliases.zsh
@@ -15,6 +15,7 @@ alias gs='git status'
 alias gpl='git pull'
 alias gplr='gpl --rebase'
 alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
+alias gstsh='git stash'
 
 #Development
 alias vim='nvim'
@@ -22,8 +23,9 @@ alias dc='docker-compose'
 alias nr='npm run'
 alias be='bundle exec'
 alias beg='be guard'
-alias bers='be rails s'
-alias berc='be rails c'
+alias ber='be rails'
+alias bers='ber s'
+alias berc='ber c'
 alias bes='be rspec'
 alias berp='pry -r ./config/environment'
 
@@ -33,10 +35,7 @@ alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
 alias k9='sudo kill -9'
 alias tf='tail -f'
 alias bsrv='brew services'
-# alias vapi-ssh-staging="ssh $(ruby utilities/aws_hosts.rb staging | grep vets-api-server | head -1  | awk '{print $1;}')"
-# alias vapi-ssh-dev="ssh $(ruby utilities/aws_hosts.rb dev | grep vets-api-server | head -1  | awk '{print $1;}')"
-
-
+alias kmt='tmux kill-session -t main'
 
 #Testing
 alias git='hub'
